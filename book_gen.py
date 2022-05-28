6ac52204c33c520ee3cf71f2fc6f9fedf6425062
fisch <python0fishers@gmail.com>
2022-05-28 12:20:11 +0300

    commit3

--- book_gen.py
+++ book_gen.py
@@ -3,15 +3,17 @@ from conf import MODEL
 from fields import other_fields
 OUTPUT_FILE = "output.json"
 
-def book_gen():
-    other_fields()
-    book_dict = {'model': MODEL,
-         'pk':"",
+def book_gen(am):
+    d = {}
+    for i in range(1, am+1):
+        book_dict = {'model': MODEL,
+         'pk':i,
          "fields": other_fields()}
+        d[i] = book_dict
     return d
 
-def to_json_file(book_gen):
-    with open(OUTPUT_FILE, "w") as f:
-        json.dump(book_gen(), f, indent=4, ensure_ascii=False)
+def to_json_file(g):
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+        json.dump(g, f, indent=4, ensure_ascii=False)
 
 
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from book_gen import book_gen, to_json_file
 
 
 if __name__ == "__main__":
-    n = book_gen
+    n = book_gen(2)
     to_json_file(n)
